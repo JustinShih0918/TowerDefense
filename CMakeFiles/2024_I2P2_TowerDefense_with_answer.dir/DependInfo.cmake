b7b277270eeb9b66f728e69e1d73ee0c6cfa4e84
JustinShih0918 <72808967+JustinShih0918@users.noreply.github.com>
2024-05-11 02:35:46 +0800

    init

---
+++ CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/DependInfo.cmake
@@ -0,0 +1,61 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/justinshih/college/I2P2-TowerDefense/Bullet/Bullet.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Bullet/Bullet.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Bullet/Bullet.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/Bullet/FireBullet.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Bullet/FireBullet.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Bullet/FireBullet.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/Bullet/LaserBullet.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Bullet/LaserBullet.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Bullet/LaserBullet.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/Bullet/MissileBullet.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Bullet/MissileBullet.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Bullet/MissileBullet.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/Enemy/Enemy.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Enemy/Enemy.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Enemy/Enemy.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/Enemy/PlaneEnemy.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Enemy/PlaneEnemy.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Enemy/PlaneEnemy.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/Enemy/SoldierEnemy.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Enemy/SoldierEnemy.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Enemy/SoldierEnemy.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/Enemy/TankEnemy.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Enemy/TankEnemy.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Enemy/TankEnemy.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/Engine/Allegro5Exception.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Engine/Allegro5Exception.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Engine/Allegro5Exception.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/Engine/AudioHelper.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Engine/AudioHelper.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Engine/AudioHelper.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/Engine/Collider.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Engine/Collider.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Engine/Collider.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/Engine/GameEngine.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Engine/GameEngine.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Engine/GameEngine.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/Engine/Group.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Engine/Group.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Engine/Group.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/Engine/IControl.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Engine/IControl.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Engine/IControl.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/Engine/IObject.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Engine/IObject.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Engine/IObject.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/Engine/IScene.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Engine/IScene.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Engine/IScene.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/Engine/LOG.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Engine/LOG.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Engine/LOG.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/Engine/Point.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Engine/Point.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Engine/Point.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/Engine/Resources.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Engine/Resources.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Engine/Resources.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/Engine/Sprite.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Engine/Sprite.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Engine/Sprite.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/Scene/LoseScene.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Scene/LoseScene.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Scene/LoseScene.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/Scene/PlayScene.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Scene/PlayScene.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Scene/PlayScene.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/Scene/SettingsScene.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Scene/SettingsScene.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Scene/SettingsScene.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/Scene/StageSelectScene.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Scene/StageSelectScene.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Scene/StageSelectScene.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/Scene/StartScene.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Scene/StartScene.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Scene/StartScene.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/Scene/WinScene.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Scene/WinScene.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Scene/WinScene.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/Turret/LaserTurret.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Turret/LaserTurret.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Turret/LaserTurret.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/Turret/MachineGunTurret.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Turret/MachineGunTurret.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Turret/MachineGunTurret.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/Turret/MissileTurret.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Turret/MissileTurret.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Turret/MissileTurret.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/Turret/Turret.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Turret/Turret.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Turret/Turret.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/Turret/TurretButton.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Turret/TurretButton.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/Turret/TurretButton.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/UI/Animation/DirtyEffect.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/UI/Animation/DirtyEffect.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/UI/Animation/DirtyEffect.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/UI/Animation/ExplosionEffect.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/UI/Animation/ExplosionEffect.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/UI/Animation/ExplosionEffect.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/UI/Animation/Plane.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/UI/Animation/Plane.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/UI/Animation/Plane.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/UI/Component/Image.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/UI/Component/Image.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/UI/Component/Image.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/UI/Component/ImageButton.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/UI/Component/ImageButton.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/UI/Component/ImageButton.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/UI/Component/Label.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/UI/Component/Label.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/UI/Component/Label.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/UI/Component/Slider.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/UI/Component/Slider.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/UI/Component/Slider.cpp.o.d"
+  "/Users/justinshih/college/I2P2-TowerDefense/main.cpp" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/main.cpp.o" "gcc" "CMakeFiles/2024_I2P2_TowerDefense_with_answer.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
