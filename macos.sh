f5c8dee1ba72539cf2da8c83adb4ff230411a6a1
JustinShih0918 <72808967+JustinShih0918@users.noreply.github.com>
2024-08-11 15:12:23 +0800

    add & on .sh

--- macos.sh
+++ macos.sh
@@ -1,7 +1,7 @@
-export PKG_CONFIG_PATH=/opt/homebrew/Cellar/allegro/5.2.9.1/lib:$PKG_CONFIG_PATH
-export PKG_CONFIG_PATH=/opt/homebrew/Cellar/allegro/5.2.9.1/lib/pkgconfig:$PKG_CONFIG_PATH
-cmake -DCMAKE_CXX_COMPILER=/usr/bin/g++ -
-DCMAKE_C_COMPILER=/usr/bin/gcc .
+export PKG_CONFIG_PATH=/opt/homebrew/Cellar/allegro/5.2.9.1/lib:$PKG_CONFIG_PATH &&
+export PKG_CONFIG_PATH=/opt/homebrew/Cellar/allegro/5.2.9.1/lib/pkgconfig:$PKG_CONFIG_PATH &&
+cmake -DCMAKE_CXX_COMPILER=/usr/bin/g++ - &&
+DCMAKE_C_COMPILER=/usr/bin/gcc . &&
 
 cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MAKE_PROGRAM="ninja" -G Ninja -S "$(pwd)" -B "$(pwd)/cmake-build-debug" &&
 cmake --build "$(pwd)/cmake-build-debug" --target 2024_I2P2_TowerDefense_with_answer &&
